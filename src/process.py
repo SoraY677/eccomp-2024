b329348f96a0fd6e135ca21726eb2c5f25c635ce
soray677 <soray677@outlook.com>
2024-12-01 22:35:19 +0900

    実行を仮定した関数定義

---
+++ src/process.py
@@ -0,0 +1,23 @@
+from util import logger
+
+def exec(question_id):
+  """実行
+
+  Args:
+      question_id (str): 問題ID
+  """
+  init(question_id)
+  run(question_id)
+
+def init(question_id):
+  """初期化
+
+  Args:
+      question_id (_type_): _description_
+  """
+  logger.init()
+  logger.info(f'Selected Question: {question_id}')
+
+def run(question_id):
+  logger.info('[run]')
+  logger.info('Todo')
