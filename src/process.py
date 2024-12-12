c781e76d4d7c9f3ed201d804c825741b32df2f73
soray677 <soray677@outlook.com>
2024-12-12 20:22:28 +0900

    多目的に対しては最大値を使う

--- src/process.py
+++ src/process.py
@@ -60,16 +60,21 @@ def run(question_id: str) -> None:
         logger.info(request_obj)
         responses = submition.submit(request_obj)
         logger.info(responses)
-        soreted_reponse = sorted({
-            k: v
-            for k, v in responses.items()
-            if v.get('feasible') and 'objective' in v
-        }.items(),
-                                 key=lambda x: x[1]['objective'])
+        sorted_reponse = sorted(
+            {
+                k: v
+                for k, v in responses.items()
+                if v.get('feasible') and 'objective' in v
+            }.items(),
+            key=lambda x:
+            (max(x[1]['objective'][0], x[1]['objective'][1])
+             if type(x[1]['objective']) is list else x[1]['objective']),
+            reverse=True if responses[0]['feasible']
+            and type(responses[0]['objective']) is list else False)
 
         # 実際に評価が高いものに独自評価を紐づける
         optimization_pairs = []
-        for response in soreted_reponse:
+        for response in sorted_reponse:
             index = response[0]
             optimization_pair = OptimizationPair(
                 result[index], solution.calc_score(result[index]))
